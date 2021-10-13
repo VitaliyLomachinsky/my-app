9c25dd3153216fcd7ee21f3ae222df40cf22254e
lomachinsky@ukr.net <lomachinsky@ukr.net>
2021-10-13 18:27:45 +0300

    First use class component

--- src/Friends/Friends.js
+++ src/Friends/Friends.js
@@ -1,60 +1,33 @@
 import FriendsListItem from "./FriendsList/FriendsListItem";
 import style from "./Friends.module.css";
 import * as axios from "axios";
+import { render } from "@testing-library/react";
+import React from "react";
 
-const Friends = (props) => {
-  let Users = [
-    {
-      id: 1,
-      followed: false,
-      fullName: "Vitaliy Lomachinsky",
-      location: {
-        city: "Kiev",
-        country: "Ukraine",
-      },
-      status: "Hello, a im nisan",
-    },
-    {
-      id: 2,
-      followed: true,
-      fullName: "Dima Sotnyk",
-      location: {
-        city: "Minsk",
-        country: "Belarus",
-      },
-      status: "C# visual basic 31",
-    },
-    {
-      id: 3,
-      followed: true,
-      fullName: "Oksana Maluh",
-      location: {
-        city: "San - Paulo",
-        country: "Egypt",
-      },
-      status: "Cool birthday",
-    },
-  ];
-
-  if (props.users.length === 0) {
+class Friends extends React.Component {
+  componentDidMount() {
     axios
       .get("https://social-network.samuraijs.com/api/1.0/users")
       .then((response) => {
-        debugger;
-        props.set_users(response.data.items);
+        this.props.set_users(response.data.items);
       });
   }
 
-  let newUserList = props.users.map((user) => (
-    <FriendsListItem user={user} follow_unfollow={props.follow_unfollow} />
-  ));
+  render() {
+    let newUserList = this.props.users.map((user) => (
+      <FriendsListItem
+        user={user}
+        follow_unfollow={this.props.follow_unfollow}
+      />
+    ));
 
-  return (
-    <div>
-      {newUserList}
-      <button className={style.btn}>Show more</button>
-    </div>
-  );
-};
+    return (
+      <div>
+        {newUserList}
+        <button className={style.btn}>Show more</button>
+      </div>
+    );
+  }
+}
 
 export default Friends;
--- src/Redux/Friends_reducer.js
+++ src/Redux/Friends_reducer.js
@@ -5,6 +5,39 @@ const SET_USERS = "SET_USERS";
 const initialState = {
   users: [],
 };
+let Users = [
+  {
+    id: 1,
+    followed: false,
+    fullName: "Vitaliy Lomachinsky",
+    location: {
+      city: "Kiev",
+      country: "Ukraine",
+    },
+    status: "Hello, a im nisan",
+  },
+  {
+    id: 2,
+    followed: true,
+    fullName: "Dima Sotnyk",
+    location: {
+      city: "Minsk",
+      country: "Belarus",
+    },
+    status: "C# visual basic 31",
+  },
+  {
+    id: 3,
+    followed: true,
+    fullName: "Oksana Maluh",
+    location: {
+      city: "San - Paulo",
+      country: "Egypt",
+    },
+    status: "Cool birthday",
+  },
+];
+
 
 const Friends_reducer = (state = initialState, action) => {
   switch (action.type) {
